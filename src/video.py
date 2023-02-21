30c0e128817a6577ed9d51c411e80a0a11afcbce
huangruiming <huangruiming@kuaishou.com>
2023-02-21 19:04:02 +0800

    generate rgb from text

--- src/video.py
+++ src/video.py
@@ -1,11 +1,22 @@
 import subprocess
 import shlex
 from PIL import Image, ImageDraw, ImageFont
+import hashlib
 
+
+def get_color_from_text(s):
+    value = int(hashlib.sha1(s.encode("utf-8")).hexdigest(), 16)
+    r = value % 255
+    value //= 255
+    g = value % 255
+    value //= 255
+    b = value % 255
+    return r, g, b
 
 
 def create_image_from_text(text):
-    img = Image.new('RGB', (100, 30), color=(73, 109, 137))
+    r, g, b = get_color_from_text(s=text)
+    img = Image.new('RGB', (100, 30), color=(r, g, b))
 
     fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
     d = ImageDraw.Draw(img)
