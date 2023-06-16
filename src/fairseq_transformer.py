938c1ba40c69ada4b8a7b400293f729ff2fa49c1
huangruiming <huangruiming@kuaishou.com>
2023-06-16 17:54:16 +0800

    update

--- src/fairseq_transformer.py
+++ src/fairseq_transformer.py
@@ -12,9 +12,7 @@ generator = task.build_generator([model], cfg)
 
 
 def generate_wav(text):
-    text = "小男童泪如雨下，将手上婴儿递给方子敬，哽咽道：“方大叔……我……我求求你，带我弟弟……带他去找爹爹……”"
-
     sample = TTSHubInterface.get_model_input(task, text)
     wav, rate = TTSHubInterface.get_prediction(task, model, generator, sample)
-
-    ipd.Audio(wav, rate=rate)
+    return wav
+    # ipd.Audio(wav, rate=rate)
