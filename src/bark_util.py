80fd64e3d938554688ad3e5265c68c2eb97f9236
huangruiming <huangruiming@kuaishou.com>
2023-06-17 23:01:37 +0800

    up

---
+++ src/bark_util.py
@@ -0,0 +1,57 @@
+import os
+import pkuseg
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import numpy as np
+
+from bark.generation import (
+    generate_text_semantic,
+    preload_models,
+)
+from bark.api import semantic_to_waveform
+from bark import generate_audio, SAMPLE_RATE
+
+preload_models()
+seg = pkuseg.pkuseg(user_dict='../resources/novel_dict.txt')
+
+GEN_TEMP = 0.6
+SPEAKER = "v2/zh_speaker_8"
+silence = np.zeros(int(0.25 * SAMPLE_RATE))  # quarter second of silence
+
+
+def generate_wav_for_long_form(raw_sentence):
+    pieces = []
+    sentences = get_text_of_fix_length(text=raw_sentence)
+    for sentence in sentences:
+        print(f'processing: {sentence}')
+        semantic_tokens = generate_text_semantic(
+            sentence,
+            history_prompt=SPEAKER,
+            temp=GEN_TEMP,
+            min_eos_p=0.05,  # this controls how likely the generation is to end
+        )
+
+        audio_array = semantic_to_waveform(semantic_tokens, history_prompt=SPEAKER)
+        pieces += [audio_array, silence.copy()]
+
+    return np.concatenate(pieces)
+
+
+def get_text_of_fix_length(text, size=50):
+    sentences = seg.cut(text)
+    cnt = 0
+    results = []
+    tmp = ''
+    for s in sentences:
+        if s not in '，。“！”？：’‘… ':
+            cnt += len(s)
+        if cnt >= size:
+            results.append(tmp)
+            tmp = s
+            cnt = len(s)
+        else:
+            tmp += s
+    if tmp:
+        results.append(tmp)
+    return results
